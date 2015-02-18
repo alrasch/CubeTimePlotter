a19ba25a4b8e7e0f61d46f0e990a73d8b823cdb0
Aleksander Rasch <a.rasch@sportradar.com>
2015-02-18 14:41:35 +0100

    Helper is no longer a class. Refactored some functions into methods.

--- src/CubeTimePlotter.py
+++ src/CubeTimePlotter.py
@@ -1,7 +1,79 @@
+from reportlab.graphics.samples.scatter import Scatter
+import Helper
+
 __author__ = 'aleksander'
-from Helper import Helper
 import plotly.plotly as py
 from plotly.graph_objs import *
+
+
+def load_file():
+    try:
+        filepath = "/home/aleksander/Dropbox/Aleks/Cubing/Times/20150218/3x3.csv"
+        with open(filepath) as f:
+            content = f.readlines()
+        return content
+    except TypeError as e:
+        print "A TypeError occurred while trying to load file.\nMessage: " + e.message
+    except Exception as e:
+        print "An exception occurred while trying to load file.\nMessage: " + e.message
+
+
+def get_times():
+    content = load_file()
+
+    times = []
+
+    for line in content:
+        time = line[22:]
+        min = time[0:2]
+        sec = time[3:5]
+        centi = time[6:8]
+        seccenti = sec + "." + centi
+
+        try:
+            seccenti = float(seccenti)
+            min = float(min)
+            addsec = 0
+            if min > 0:
+                addsec = min * 60
+            times.append(float(seccenti + addsec))
+        except ValueError as e:
+            print "ValueError in get_times(): " + e.message
+        except Exception as e:
+            print "Exception in get_times(): " + e.message
+
+    #Reverse the list because Speedcube Timer lists them from last to first
+    times.reverse()
+
+    return times
+
+
+def plot():
+
+    #Make a trace for the times
+    times = get_times()
+    x = []
+    for i in range(1, len(times) + 1):
+        x.append(i)
+    trace1 = Scatter(x=x, y=times)
+
+    #Make a trace for the average of last 5
+    averages = [0, 0, 0, 0]
+    for i in range(4, len(times)):
+        subtable = []
+        for j in range(i-4, i+1):
+            subtable.append(times[j])
+        avg = Helper.get_avg(subtable)
+        averages.append(avg)
+
+    x = []
+    for i in range(1, len(averages)+1):
+        x.append(i)
+
+    trace2 = Scatter(x=x, y=averages)
+
+    data = Data([trace1, trace2])
+    py.plot(data, filename='basic-line')
 
 
 class CubeTimePlotter:
@@ -11,79 +83,10 @@ class CubeTimePlotter:
     file = None
 
     def __init__(self):
-        self.helper = Helper()
-        self.times = self.get_times()
+        self.times = get_times()
         self.init()
 
     def init(self):
-        self.plot()
-        print "Lifetime average: " + self.helper.get_lifetime_avg(self.times).__str__()
-        print "Lifetime best:    " + self.helper.get_lifetime_best(self.times).__str__()
-
-    def load_file(self):
-        try:
-            filepath = "/home/aleksander/Dropbox/Aleks/Cubing/Times/20150218/3x3.csv"
-            with open(filepath) as f:
-                content = f.readlines()
-            return content
-        except TypeError as e:
-            print "A TypeError occurred while trying to load file.\nMessage: " + e.message
-        except Exception as e:
-            print "An exception occurred while trying to load file.\nMessage: " + e.message
-
-
-    def get_times(self):
-        content = self.load_file()
-
-        times = []
-
-        for line in content:
-            time = line[22:]
-            min = time[0:2]
-            sec = time[3:5]
-            centi = time[6:8]
-            seccenti = sec + "." + centi
-
-            try:
-                seccenti = float(seccenti)
-                min = float(min)
-                addsec = 0
-                if min > 0:
-                    addsec = min * 60
-                times.append(float(seccenti + addsec))
-            except ValueError as e:
-                print "ValueError in get_times(): " + e.message
-            except Exception as e:
-                print e.__class__ + " in get_times(): " + e.message
-
-        #Reverse the list because Speedcube Timer lists them from last to first
-        times.reverse()
-
-        return times
-
-    def plot(self):
-
-        #Make a trace for the times
-        times = self.get_times()
-        x = []
-        for i in range(1, len(times) + 1):
-            x.append(i)
-        trace1 = Scatter(x = x, y = times)
-
-        #Make a trace for the average of last 5
-        averages = [0, 0, 0, 0]
-        for i in range(4, len(times)):
-            subtable = []
-            for j in range(i-4, i+1):
-                subtable.append(times[j])
-            avg = self.helper.get_avg(subtable)
-            averages.append(avg)
-
-        x = []
-        for i in range(1, len(averages)+1):
-            x.append(i)
-
-        trace2 = Scatter(x = x, y = averages)
-
-        data = Data([trace1, trace2])
-        plot_url = py.plot(data, filename = 'basic-line')
+        plot()
+        print "Lifetime average: " + Helper.get_lifetime_avg(self.times).__str__()
+        print "Lifetime best:    " + Helper.get_lifetime_best(self.times).__str__()
--- src/Helper.py
+++ src/Helper.py
@@ -1,54 +1,52 @@
 __author__ = 'aleksander'
 
 
-class Helper:
+def get_lifetime_best(times):
+    best = None
+    for time in times:
+        if best is None:
+            best = time
+        elif time < best:
+            best = time
+    if best is not None:
+        return best
+    return False
 
-    def __init__(self):
-        pass
 
-    def get_lifetime_best(self, times):
-        best = None
-        for time in times:
-            if best == None:
-                best = time
-            elif time < best:
-                best = time
-        if best != None:
-            return best
-        return False
+def get_avg(table):
+    if len(table) == 0:
+        return None
+    sum = 0
+    for value in table:
+        try:
+            sum += value
+        except Exception as e:
+            print "Error in getting average of table: " + e.__str__()
+    avg = sum/len(table)
+    return avg
 
-    def get_avg(self, table):
-        if len(table) == 0:
-            return None
-        sum = 0
-        for value in table:
-            try:
-                sum += value
-            except Exception as e:
-                print "Error in getting average of table: " + e.__str__()
-        avg = sum/len(table)
-        return avg
 
-    def avg_of_last_n(self, n, times):
-        last_n = times[-1*n:]
+def avg_of_last_n(n, times):
+    last_n = times[-1*n:]
 
-        sum = 0
-        for time in last_n:
+    sum = 0
+    for time in last_n:
+        sum += time
+
+    avg = sum / len(last_n)
+
+    return avg
+
+
+def get_lifetime_avg(times):
+    if len(times) < 1:
+        return None
+    sum = 0
+    for time in times:
+        try:
             sum += time
+        except Exception as e:
+            print "Error in getting lifetime average: " + e.__str__()
 
-        avg = sum / len(last_n)
-
-        return avg
-
-    def get_lifetime_avg(self, times):
-        if len(times) < 1:
-            return None
-        sum = 0
-        for time in times:
-            try:
-                sum += time
-            except Exception as e:
-                print "Error in getting lifetime average: " + e.__str__()
-
-        avg = sum/len(times)
-        return avg
+    avg = sum/len(times)
+    return avg
